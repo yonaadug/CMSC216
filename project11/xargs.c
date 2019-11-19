02dd4c19d1ef0bff15ee013b5f1d316445a2badf
root <root@Prime.localdomain>
2019-11-19 05:44:11 -0500

    Project 11: Testing after complete

--- project11/xargs.c
+++ project11/xargs.c
@@ -208,7 +208,7 @@ void free_file_args(char **file_args) {
 }
 
 /*Counts size of the array including the NULL at the end*/
-int count_size(char *arr[]) {
+int count_size(char **arr) {
     int i = 0;
     while (arr + i*(sizeof(char *)) != NULL) {
         i++;
