c095c2de3c349e46c98a3565dbbc4f4cbebc2a24
root <root@Prime.localdomain>
2019-12-08 20:03:41 -0500

    Project 13: Complete (Including comments)

--- project13/wc-threaded.c
+++ project13/wc-threaded.c
@@ -2,8 +2,15 @@
  * Name: Yonathan A Kebede
  * UID: 115659780
  * TerpConnect: ykebed12
+ * Section: 0302
  * 
+ * Description: This program replicates the Unix program "wc"
+ *              that calculates the total word count, character
+ *              count and line count in the arguments, or files
+ *              given.
  * 
+ * I pledge on my honor that I have not given or recieved
+ * any unauthorized assistance on this examination or assignment.
  */ 
 
 #include <stdio.h>
@@ -11,67 +18,105 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-void *count(void *filename);
+/*
+ * This function counts the words, lines and
+ * characters in each file passed to this program
+ * and returns it through the argument passed
+ * 
+ */ 
+void *count(void *file_and_args);
 
-
-
+/*
+ * Struct for the arguments passed into
+ * the thread
+ */
 struct thread_args {
-    int lines;
-    int words;
-    int chars;
+    int *lines;
+    int *words;
+    int *chars;
+    int id;
     char *file;
        
 };
 
+/* Main Function */
 int main (int argc, char *argv[]) {
 
+    /* variables that will hold total words, lines and chars*/
     int total_lines = 0, total_words = 0, total_chars = 0;
-    int arg_num = 1;
+
+    int arg_num = 1, i;
     pthread_t *thread_id;
 
     if (argc > 1) {
 
+        /* Allocate memory for the args into thread*/
         struct thread_args *t_args = malloc(sizeof(struct thread_args));
-        thread_id = malloc(sizeof(pthread_t)*(argc-1));
+        t_args->chars = malloc(sizeof(int)* (argc-1));
+        t_args->words = malloc(sizeof(int)* (argc-1));
+        t_args->lines = malloc(sizeof(int)* (argc-1));
 
+        /* Allocate memory for thread id's for different files */
+        thread_id = malloc(sizeof(pthread_t) * (argc-1));
+
+
+        /* Loop through each argument in argv to pass into thread */
         while (arg_num < argc) {
 
+            /* Pass file name and 'id' index */
             t_args->file = argv[arg_num];
-
+            t_args->id = arg_num-1;
+            
             pthread_create(&thread_id[arg_num-1], NULL, &count, t_args);
-            pthread_join(thread_id[arg_num-1], NULL);
-
-            total_chars += t_args->chars;
-            total_lines += t_args->lines;
-            total_words += t_args->words;
-
+        
             arg_num++;
             
         }
 
+        /* Get results from each thread */
+        for (i = 0; i < argc-1; i++) {
+            pthread_join(thread_id[arg_num-1], NULL);
+
+            /* Increment totals */
+            total_lines += t_args->lines[i];
+            total_chars += t_args->chars[i];
+            total_words += t_args->words[i];
+
+        }
+
+        /* Free all allocated memory*/
+        free(t_args->chars);
+        free(t_args->words);
+        free(t_args->lines);
         free(t_args);
         free(thread_id);
 
     }
 
+    /* print results */
     printf("%4d %4d %4d\n", total_lines, total_words, total_chars);
     
 
     return 0;
 }
-
-
+ 
 
 void *count(void *file_and_args){
 
     FILE *fp;
+
+    /* Get the struct args*/
     struct thread_args *t_args = file_and_args; 
-    char ch, next_ch, *filename = t_args->file;
-    int lines = 0, words = 0, chars = 0; 
+    char *filename = t_args->file;
+    int id = t_args->id;
+
+    char ch, next_ch;
+    /*Variables used for counting lines words and chars*/
+    int lines = 0, words = 0, chars = 0;
 
     fp= fopen(filename, "r");  /* open that file */
 
-        /* silently ignore any problems trying to open files */
+    /* silently ignore any problems trying to open files */
     if (fp != NULL) {
 
         /* read each file one character at a time, until EOF */
@@ -101,13 +146,14 @@ void *count(void *file_and_args){
             ch = fgetc(fp);
         }
 
-
+        /* close the file */
         fclose(fp);
     }
 
-    t_args->lines = lines;
-    t_args->words = words;
-    t_args->chars = chars;
+    /*Return the lines, words and chars in the file*/
+    t_args->lines[id] = lines;
+    t_args->words[id] = words;
+    t_args->chars[id] = chars;
 
     return NULL;
 }
